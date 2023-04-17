39adca3cc6b2a37223aa3bfbf11c55b315e4c30a
Lawrence Akpan <delaw34@yahoo.com>
2023-04-17 23:48:08 +0100

    Update sum_cal.py

--- sum_cal.py
+++ sum_cal.py
@@ -1,15 +1,11 @@
-#Calculating a running sum
-#A user will enter numbers that will be added to the sum 
-# when a negative number is encountered stop adding numbers and write out the final result
-# sum = 0
-number = +0
 
-def sum_cal():
-    sum_calculation = int(input('enter the sum: '))
-    if sum_calculation > 0 :
-        final_sum = number + sum_calculation
-        print('the running sum is', final_sum )
-    else:
-        final_sum = number + sum_calculation
-        print('the running sum total is', final_sum, 'only positive numbers are required')
-sum_cal()        
+# Python program that sums up all the items in a list of 9 items
+# remove duplicates
+# sums up the list
+# print the result
+
+
+numbers = [12, 14, 16, 18, 20,10,12, 20]
+remove_dul = set(numbers)
+sum_total = sum(remove_dul)
+print("this is total sum of all numbers after removing all duplicates", sum_total)
